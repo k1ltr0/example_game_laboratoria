cb303fe0785c277a0f85b514f81a75a8fd3a5915
Natalia Garrido <natgramirez@outlook.com>
2017-08-09 18:23:26 -0400

    agregando flecha y seleccion de personajes

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-'use strict';
-
-
 window.addEventListener('load', function() {
 
     // princess
     var game = new Game([
+        new ExampleMap0(),
         new ExampleMap1(),
         new ExampleMap2(),
         new ExampleMap3()
     ], [
-        //Princess
-        //Hero
+        Arrow,
+        Princess,
+        KnightHero,
         Fox
     ]);
 
